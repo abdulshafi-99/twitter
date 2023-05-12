a7788563f9308693a307f14b22fa51361db13ca0
abdulshafi-99 <abdulshafi.alwahsh@gmail.com>
2023-05-12 18:21:52 +0300

    Sidebar

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,9 @@
+export function NewTweetForm() {
+    return (
+        <form className="flex flex-col gap-2 border-b px-4 py-2">
+            <div className="flex gap-4">
+                
+            </div>
+        </form>
+    )
+}
